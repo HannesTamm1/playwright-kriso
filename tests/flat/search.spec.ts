e458b71a4f0f6f51eaa1ba84a574db4214e0d7a0
Hannes Tamm <hannes.tamm@tptlive.ee>
2026-05-27 14:35:33 +0300

    Test: add search flat test

--- tests/flat/search.spec.ts
+++ tests/flat/search.spec.ts
@@ -9,10 +9,28 @@
  * Tip: run `npx playwright codegen https://kaup24.ee/et/` to discover selectors.
  */
 import { test, expect } from '@playwright/test';
-import type { Page } from '@playwright/test';
 
+test('Kodushop otsingu testimine', async ({ page }) => {
 
+  // --- Ava kodushop.ee ---
+  await page.goto('https://kodushop.ee/');
+  await expect(page.getByRole('link', { name: 'kodushop.ee' })).toBeVisible();
 
-test.describe('', () => {
+  await page.getByRole('banner').getByRole('textbox', { name: 'Search' }).fill('xqfasdfjl');
+    await page.getByRole('banner').getByRole('button', { name: 'Search ' }).click();
+  await page.waitForLoadState('networkidle');
+  const pageContent = await page.content();
+  const noResults = await page.locator('.product').count();
+  expect(noResults).toBe(0);
 
-}); 
+  await page.getByRole('banner').getByLabel('Puhasta otsing').click();
+  await page.getByRole('banner').getByRole('textbox', { name: 'Search' }).fill('grill');
+  await page.getByRole('banner').getByRole('button', { name: 'Search ' }).click();
+  await expect(page.getByText('Quick view Grillkinnas, hõbe')).toBeVisible();
+  await expect(page.getByText('Esperanza võileivagrill 1000W, roostevaba')).toBeVisible();
+
+  await page.getByRole('banner').getByRole('textbox', { name: 'Search' }).fill('5901299954843');
+  await page.getByRole('banner').getByRole('button', { name: 'Search ' }).click();
+  await expect(page.getByRole('heading', { name: 'Esperanza võileivagrill 1000W' })).toBeVisible();
+
+});
